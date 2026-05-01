0068929fcf22afc69488eed6d255a88c7acca41a
Meenakshi <meenakshichhonker@gmail.com>
2026-05-01 15:17:11 +0530

    base url addes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://team-task-manager-fd7s.onrender.com/',
   headers: {
     'Content-Type': 'application/json'
   }
